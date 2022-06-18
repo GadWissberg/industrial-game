cedc283cdfc900770839e2dd2c2538e0ee306cfa
Gad Wissberg <gadw17@gmail.com>
2022-06-18 23:15:35 +0300

    Switching to industrial.

--- core/src/com/gadarts/industrial/DefaultGameSettings.java
+++ core/src/com/gadarts/industrial/DefaultGameSettings.java
@@ -22,7 +22,7 @@ public final class DefaultGameSettings {
 	public static final boolean DEBUG_INPUT = false;
 	public static final boolean DISABLE_FRUSTUM_CULLING = false;
 	public static final int LOG_LEVEL = LOG_DEBUG;
-	public static final WeaponsDefinitions STARTING_WEAPON = WeaponsDefinitions.HAMMER;
+	public static final WeaponsDefinitions STARTING_WEAPON = WeaponsDefinitions.COLT;
 	public static final boolean PARALYZED_ENEMIES = false;
 
 	public static final boolean ALLOW_STATIC_SHADOWS = true;
